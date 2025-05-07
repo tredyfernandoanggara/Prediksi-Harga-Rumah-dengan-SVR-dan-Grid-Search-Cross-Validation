0473b30cf0e3efda87d4ffe1f57b723a77cf5d3e
T. REDY FERNANDO ANGGARA <keyline990@gmail.com>
2025-05-07 14:22:53 +0700

    Melakukan prediksi untuk 1 tahun kedepan, data yang digunakan adalah data yang sudah ada dan mengubah tahun nya menjadi 2024

--- myplot.py
+++ myplot.py
@@ -28,3 +28,40 @@ def reg_plot(data, x, y, title=None):
     plt.grid()
     plt.tight_layout()
     plt.show()
+
+# Group By Plot
+def group_plot(data_group, x, y, title=None):
+    num_groups = len(data_group)
+    num_rows = (num_groups + 1) // 2  # Memastikan jumlah baris cukup
+
+    figsize_x, figsize_y = 12, 4 * num_rows  # Lebar tetap, tinggi menyesuaikan
+    fig, axs = plt.subplots(nrows=num_rows, ncols=2, figsize=(figsize_x, figsize_y))
+    axs = axs.flatten()
+
+    # Title
+    fig.suptitle(title, fontsize=16, y=1)
+    
+    # Looping setiap kelompok
+    for ax, (key, group) in zip(axs, data_group):
+        ax.plot(group[x], group[y], marker='o', linestyle='-')
+        ax.set_title(f'{key}', fontsize=12)
+        ax.set_xlabel(x)
+        ax.set_ylabel(y)
+
+        # Menentukan batas X dan Y secara otomatis
+        min_x, max_x = group[x].min(), group[x].max()
+        min_y, max_y = group[y].min(), group[y].max()
+    
+        ax.set_xlim(min_x - 1, max_x + 1)  # Ruang ekstra X
+        ax.set_ylim(min_y - 10, max_y + 10)  # Ruang ekstra y
+
+        # Menambahkan teks dengan jarak lebih baik
+        for X, Y in zip(group[x], group[y]):
+            ax.annotate(f'{Y:.2f}', (X, Y), textcoords="offset points", xytext=(0, 5), ha='center')
+
+    # Menyembunyikan subplot yang tidak terpakai
+    for j in range(num_groups, len(axs)):
+        fig.delaxes(axs[j])
+
+    plt.tight_layout()
+    plt.show()
