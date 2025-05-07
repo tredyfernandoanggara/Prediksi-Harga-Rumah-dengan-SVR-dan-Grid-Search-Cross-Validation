06c21aab34712fffb878a3ce4c4eb7b7914fb6a7
T. REDY FERNANDO ANGGARA <keyline990@gmail.com>
2025-05-07 11:11:56 +0700

    Mengambil dataset yang akan digunakan untuk melakukan prediksi harga rumah, dataset yang digunakan berjumlah 60 data, dengan kolom yang terdiri dari: tipe, tahun, luas bangunan, luas tanah, jumlah kamar tidur, jumlah kamar mandi, garasi, harga, dan lokasi

---
+++ myplot.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Barplot
+def bar_plot(x, y, hue=None, title=None):
+    ax = sns.barplot(x=x, y=y, hue=hue)
+    
+    for p in ax.patches:
+        # Mendapatkan teks
+        text = int(p.get_height())
+        
+        if p.xy != (0, 0):
+            # Annotate text
+            ax.text(x=p.get_x() + p.get_width() / 2, y=p.get_height(),
+                    s=text,
+                    ha='center', va='bottom', fontsize=10, color='black')
+    # Title
+    plt.title(title)
+    # Layout
+    plt.tight_layout()
+    plt.show()
